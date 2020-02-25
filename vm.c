eb55bd602904d24273c9fc65ae3b551c8a2b2c8d
samuelweekes <samueljweekes@gmail.com>
2020-02-25 20:41:59 +0000

    Added switch structure for op codes

--- vm.c
+++ vm.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <signal.h>
-/* unix */
-#include <unistd.h>
-#include <fcntl.h>
-
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/termios.h>
-#include <sys/mman.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdint.h>
+//#include <string.h>
+//#include <signal.h>
+///* unix */
+//#include <unistd.h>
+//#include <fcntl.h>
+//
+//#include <sys/time.h>
+//#include <sys/types.h>
+//#include <sys/termios.h>
+//#include <sys/mman.h>
 
 /* 65536 locations */
 uint16_t memory[UINT16_MAX];
@@ -57,3 +57,84 @@ enum
   FL_ZRO = 1 << 1,
   FL_NEG = 1 << 2
 };
+
+int main(int argc, const char* argv[]){
+  
+  if(argc < 2){
+    /* show usage string */
+    printf("lc3 [image-file1] ... \n");
+    exit(2);
+  }
+
+  for(int j = 1; j < argc; ++j)
+  {
+    if(!read_image(argv[j]))
+    {
+      printf("failed to load image: %s\n", argv[j]);
+      exit(1);
+    } 
+  }
+
+  enum {PC_START = 0x3000};  
+  reg[R_PC] = PC_START;
+
+  int running = 1;
+  while (running)
+  {
+    /* FETCH */
+    uint16_t instr = mem_read(reg[R_PC]++);
+    uint16_t op = instr >> 12;
+
+    switch (op)
+    {
+      case OP_ADD:
+        {ADD, 6}
+        break;
+      case OP_AND:
+        {AND, 7}
+        break;
+      case OP_NOT:
+        {NOT, 7}
+        break;
+      case OP_BR:
+        {BR, 7}
+        break;
+      case OP_JMP:
+        {JMP, 7}
+        break;
+      case OP_JSR:
+        {JSR, 7}
+        break;
+      case OP_LD:
+        {LD, 7}
+        break;
+      case OP_LDI:
+        {LDI, 7}
+        break;
+      case OP_LDR:
+        {LDR, 7}
+        break;
+      case OP_LEA:
+        {LEA, 7}
+        break;
+      case OP_ST:
+        {ST, 7}
+        break;
+      case OP_STI:
+        {STI, 7}
+        break;
+      case OP_STR:
+        {STR, 7}
+        break;
+      case OP_TRAP:
+        {TRAP, 8}
+        break;
+      case OP_RES:
+      case OP_RTI:
+      default:
+        {BAD OPCODE, 7}
+        break;
+    }
+  }
+  {Shutdown, 12}
+}
